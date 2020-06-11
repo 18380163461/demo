f8640b5b14454ddd88fa018174d3ac2cce757b41
youpd <youpd@asiainfo.com>
2020-06-11 18:51:21 +0800

    登录

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,12 +1,11 @@
 package com.example.demo.controller;
 
-import com.example.demo.constants.BusinessConstants;
-import com.example.demo.model.BaseResponse;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.example.demo.dao.mapper.bo.User;
 import java.util.HashMap;
 import java.util.Map;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,26 +17,16 @@ public class LoginController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
-  @GetMapping("login")
-  public BaseResponse login(HttpServletResponse httpServletResponse, HttpServletRequest httpServletRequest,
-      String userName, String passWord) {
-    BaseResponse response = new BaseResponse(true, BusinessConstants.BUSI_SUCCESS_CODE, BusinessConstants.BUSI_SUCCESS_MESSAGE);
-    httpServletRequest.getCookies();
-    HttpSession httpSession = httpServletRequest.getSession();
-    if (passWord.equals("123456")) {
-      Cookie cookie = new Cookie("userName", userName);
-      cookie.setMaxAge(60 * 60 * 24 * 7);
-      httpServletResponse.addCookie(cookie);
-      return response;
-    }
-    return response;
-  }
 
   @GetMapping(value = "/session")
   public Map<String, Object> getSession(HttpServletRequest request, String userName, String passWord) {
     // 添加数据到Session
     HttpSession httpSession = request.getSession();
-    httpSession.setAttribute("username", userName);
+    User user = new User();
+    user.setName(userName);
+    user.setAge(99);
+    user.setId(1);
+    httpSession.setAttribute("userInfo", JSON.toJSONString(user));
     // 添加sessionID到Map
     Map<String, Object> map = new HashMap<>();
     map.put("sessionId", request.getSession().getId());
@@ -48,10 +37,13 @@ public class LoginController {
   public Map get(HttpServletRequest request) {
     // 获取Session数据
     HttpSession httpSession = request.getSession();
-    String userName = (String) httpSession.getAttribute("username");
+    String userInfo = (String) httpSession.getAttribute("userInfo");
+    User user = JSON.parseObject(userInfo, new TypeReference<User>() {
+    });
+    LOGGER.debug("{}", user);
     Map<String, Object> map = new HashMap<>();
     map.put("sessionId", httpSession.getId());
-    map.put("username", userName);
+    map.put("userInfo", userInfo);
     return map;
   }
 
